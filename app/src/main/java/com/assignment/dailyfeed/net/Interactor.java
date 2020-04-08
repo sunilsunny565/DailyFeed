47b49284c3b5677511eb954ff498cd41239aa4b2
sunil <sunilsunny565@gmail.com>
2020-04-08 13:41:04 +1000

    Unit test for checking if correct number of items are returned.

--- app/src/main/java/com/assignment/dailyfeed/net/Interactor.java
+++ app/src/main/java/com/assignment/dailyfeed/net/Interactor.java
@@ -8,20 +8,35 @@ import com.assignment.dailyfeed.model.FeedModel;
 import com.assignment.dailyfeed.model.FeedResponse;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
 import java.util.ArrayList;
 import java.util.List;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 import static com.assignment.dailyfeed.model.FeedResponse.BASE_URL;
 
-public class Interactor implements GetDataContract.Interactor{
+public class Interactor implements GetDataContract.Interactor {
     private GetDataContract.onGetDataListener mOnGetDatalistener;
     private List<FeedItem> feeds = new ArrayList<>();
 
+    public List<FeedItem> getFeeds() {
+        return feeds;
+    }
 
-    Interactor(GetDataContract.onGetDataListener mOnGetDatalistener) {
+    public Interactor(GetDataContract.onGetDataListener mOnGetDatalistener) {
         this.mOnGetDatalistener = mOnGetDatalistener;
+    }
+    public void fetchFeedItems(String title, List<FeedItem> feedItems) {
+        for (FeedItem feedItem : feedItems) {
+            if (feedItem.getDescription() != null && feedItem.getImageHref() != null && feedItem.getDescription() != null) {
+                feeds.add(feedItem);
+            }
+        }
+        if (feeds != null) {
+            mOnGetDatalistener.onSuccess(title, feeds);
+        }
     }
 
     @Override
@@ -39,16 +54,8 @@ public class Interactor implements GetDataContract.Interactor{
             @Override
             public void onResponse(retrofit2.Call<FeedModel> call, retrofit2.Response<FeedModel> response) {
                 FeedModel jsonResponse = response.body();
-                if(jsonResponse != null && jsonResponse.getFeedItems() != null){
-                    for (FeedItem feedItem: jsonResponse.getFeedItems()) {
-                        if(feedItem.getDescription() != null && feedItem.getImageHref()!= null && feedItem.getDescription() !=null){
-                            feeds.add(feedItem) ;
-                        }
-                    }
-                    if(feeds != null){
-                        Log.d("Data", "Refreshed");
-                        mOnGetDatalistener.onSuccess(jsonResponse.getTitle(), feeds);
-                    }
+                if (jsonResponse != null && jsonResponse.getFeedItems() != null) {
+                    fetchFeedItems(jsonResponse.title, jsonResponse.getFeedItems());
                 }
             }
 
--- app/src/test/java/com/assignment/dailyfeed/ExampleUnitTest.java
+++ app/src/test/java/com/assignment/dailyfeed/ExampleUnitTest.java
@@ -1,8 +1,16 @@
 package com.assignment.dailyfeed;
 
+import com.assignment.dailyfeed.model.FeedItem;
+import com.assignment.dailyfeed.net.GetDataContract;
+import com.assignment.dailyfeed.net.Interactor;
+
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -14,4 +22,37 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void test_returningCorrectObject(){
+        GetDataContract.onGetDataListener mOnGetDatalistener = new GetDataContract.onGetDataListener() {
+            @Override
+            public void onSuccess(String appTitle, List<FeedItem> list) {
+                assertEquals(list.size(),9);
+            }
+
+            @Override
+            public void onFailure(String message) {
+
+            }
+        };
+        Interactor interactor = new Interactor(mOnGetDatalistener);
+        interactor.fetchFeedItems("",generateFeedItems());
+    }
+    private List<FeedItem> generateFeedItems()
+    {
+        List<FeedItem> feedItems = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            FeedItem feedItem = new FeedItem();
+            if(i != 5) {
+                feedItem.setTitle("FeedTitle" + i);
+                feedItem.setDescription("description" + i);
+                feedItem.setImageHref("image" + i);
+                feedItems.add(feedItem);
+            }
+        }
+        return feedItems;
+    }
+
+
 }
